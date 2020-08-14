c34e91369363d5b5e4d7adee8dffa1547b8bac3c
Deus Intra <deusintra@yahoo.com>
2020-08-15 04:34:19 +0500

    found a bug. will fix later maybe

--- Novel/Assets/Scripts/DialogBox/Text Animations/TextNoAnimation.cs
+++ Novel/Assets/Scripts/DialogBox/Text Animations/TextNoAnimation.cs
@@ -2,6 +2,8 @@
 using UnityEngine;
 using Novella.Dialog;
 
+// has a bug: if no animation is selected, text effects don't apply on first quote.
+// fixing it is not a priority
 public class TextNoAnimation : MonoBehaviour, ITextAnimation
 {
     public bool isAnimating()
